e68da086985baf9c5c5e49559d0fc92ff515f70e
Barath B <barath@Baraths-MacBook-Air.local>
2024-10-15 01:03:15 +0530

    Fixed Static Error 500

--- iqac/settings.py
+++ iqac/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 ROOT_URLCONF = 'iqac.urls'
 
