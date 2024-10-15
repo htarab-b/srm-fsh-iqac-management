9323b78d76503e115bb2275c48a7e36637be7d98
Barath B <barath@Baraths-MacBook-Air.local>
2024-10-15 22:06:25 +0530

    Modified vercel json to attempt to fix static files bug

--- iqac/settings.py
+++ iqac/settings.py
@@ -141,10 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
